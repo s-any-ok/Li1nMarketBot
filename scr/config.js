1a667fd7a627993e2de4789bb40dd348dcec0ef8
Oleksandr <sashavytvyckyj@gmail.com>
2020-05-03 19:05:00 +0300

    update

--- scr/config.js
+++ scr/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     TOKEN: process.env.TELEGRAM_TOKEN || '1000060464:AAE6bwWUhoGttztN1KxjeNtyDsZuYLiB5rs',
-    DB_URL: process.env.MONGODB_URI || 'mongodb://user:l1on2020@ds333768.mlab.com:33768/heroku_tnnf7813'
+    DB_URL: process.env.MONGODB_URI || 'mongodb://heroku_tnnf7813:onl0g3ii2m0l3cvfki1onaanbn@ds333768.mlab.com:33768/heroku_tnnf7813'
 }
