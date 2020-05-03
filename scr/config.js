fd1a9902a89c1acf063f841071ea045b085ac4ce
Oleksandr <sashavytvyckyj@gmail.com>
2020-05-03 18:39:14 +0300

    update

--- scr/config.js
+++ scr/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     TOKEN: process.env.TELEGRAM_TOKEN || '1000060464:AAE6bwWUhoGttztN1KxjeNtyDsZuYLiB5rs',
-    DB_URL: process.env.MONGOLAB_URI || 'mongodb://<dbuser>:<dbpassword>@ds333768.mlab.com:33768/heroku_tnnf7813'
+    DB_URL: 'mongodb://<dbuser>:<dbpassword>@ds333768.mlab.com:33768/heroku_tnnf7813'
 }
