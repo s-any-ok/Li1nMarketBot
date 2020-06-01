93260e16c333b16e06ec7376741e528a9847aef3
OleksandrLevak <sashavytvyckyj@gmail.com>
2020-06-01 16:50:01 +0300

    update database

--- scr/index.js
+++ scr/index.js
@@ -10,7 +10,7 @@ const helpers = require('./helpers');
 const kb = require('./button');
 const keyboard = require('./keyboard');
 //const database = require('../databases/database.json');
-const atbDatabase = require('../databases/atb-database.json');
+//const atbDatabase = require('../databases/atb-database.json');
 
 const options = {
   webHook: { port: process.env.PORT },
@@ -38,7 +38,7 @@ const Shop = require('./models/shop.model');
 const User = require('./models/user.model');
 
 //database.products.forEach(p => new Product(p).save());
-atbDatabase.atbProducts.forEach(p => new AtbProduct(p).save());
+//atbDatabase.atbProducts.forEach(p => new AtbProduct(p).save());
 
 bot.on('message', msg => {
 
